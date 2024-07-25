085c90ca488216df535deee3e7f7e2c600da3cc5
developerNoman <noman.shafique@rakurai.io>
2024-07-25 10:48:18 +0500

    updated files

--- clientServer/server.py
+++ clientServer/server.py
@@ -36,4 +36,4 @@ def data():
     return jsonify({"error": "Invalid request"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=False)
